253d17fc611ca116cd02cda021204c53fef5b549
ChoihaLam <Choihalam@foxmail.com>
2016-08-17 10:25:36 +0800

    代码完成

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@
  */
 var chessBoard = [];
 var me = true;
-//Ó®·¨Êý×é
+var over = false;
+//èµ¢æ³•æ•°ç»„
 var wins = [];
+
+//èµ¢æ³•çš„ç»Ÿè®¡æ•°ç»„
+var myWin = [];
+var computerWin = [];
 
 for(var i=0; i<15; i++){
     chessBoard[i] = [];
@@ -21,7 +26,7 @@ for(var i=0; i<15; i++){
 }
 
 var count = 0;
-//ËùÓÐµÄºáÏß
+//æ‰€æœ‰çš„æ¨ªçº¿
 for(var i=0; i<15; i++){
     for(var j=0; j<11; j++){
         // wins[0][0][0] = true;
@@ -41,7 +46,7 @@ for(var i=0; i<15; i++){
         count++;
     }
 }
-//ËùÓÐµÄÊúÏß
+//æ‰€æœ‰çš„ç«–çº¿
 for(var i=0; i<15; i++){
     for(var j=0; j<11; j++){
         for(var k=0; k<5; k++){
@@ -50,7 +55,7 @@ for(var i=0; i<15; i++){
         count++;
     }
 }
-//ËùÓÐµÄÐ±Ïß
+//æ‰€æœ‰çš„æ–œçº¿
 for(var i=0; i<11; i++){
     for(var j=0; j<11; j++){
         for(var k=0; k<5; k++){
@@ -59,7 +64,7 @@ for(var i=0; i<11; i++){
         count++;
     }
 }
-//ËùÓÐµÄ·´Ð±Ïß  Ð´µ½ÕâÀï---------------------------
+//æ‰€æœ‰çš„åæ–œçº¿  
 for(var i=0; i<11; i++){
     for(var j=14; j>3; j--){
         for(var k=0; k<5; k++){
@@ -67,6 +72,13 @@ for(var i=0; i<11; i++){
         }
         count++;
     }
+}
+
+console.log(count);
+
+for(var i=0; i<count; i++){
+    myWin[i] = 0;
+    computerWin[i] = 0;
 }
 
 var chess = document.getElementById('chess');
@@ -77,18 +89,18 @@ context.strokeStyle = "#BFBFBF";
 var logo = new Image();
 logo.src = "images/logo.png";
 logo.onload = function(){
-    context.drawImage(logo, 0, 0, 450, 450);  // »­±³¾°Í¼Æ¬
-    drawChessBoard();  //µ÷ÓÃÆå°åº¯Êý
+    context.drawImage(logo, 0, 0, 450, 450);  // ç”»èƒŒæ™¯å›¾ç‰‡
+    drawChessBoard();  //è°ƒç”¨æ£‹æ¿å‡½æ•°
 }
 
 
 
 var drawChessBoard = function(){
     for(var i=0; i<15; i++){
-        context.moveTo(15+i*30,15);    //»­ÊúÏß
+        context.moveTo(15+i*30,15);    //ç”»ç«–çº¿
         context.lineTo(15+i*30,435);
         context.stroke();
-        context.moveTo(15,15+i*30);     //»­ºáÏß
+        context.moveTo(15,15+i*30);     //ç”»æ¨ªçº¿
         context.lineTo(435,15+i*30);
         context.stroke();
 
@@ -96,7 +108,7 @@ var drawChessBoard = function(){
 }
 
 var oneStep = function(i,j,me){
-    context.beginPath();  //»­Æå×Ó
+    context.beginPath();  //ç”»æ£‹å­
     context.arc(15 + i*30, 15 + j*30, 13, 0, 2*Math.PI);
     context.closePath();
     var gradient = context.createRadialGradient(15+ i*30+2, 15 + j*30-2, 13, 15+i*30+2, 15+j*30-2, 0);
@@ -112,20 +124,120 @@ var oneStep = function(i,j,me){
 }
 
 chess.onclick = function (e) {
+    if(over){
+        return;
+    }
+    if(!me){
+        return;
+    }
     var x = e.offsetX;
     var y = e.offsetY;
     var i = Math.floor(x / 30);
     var j = Math.floor(y / 30);
-    if(chessBoard[i][j] == 0){
-    oneStep(i,j,me);
-        if(me){
-            chessBoard[i][j] = 1;
-        }else{
-            chessBoard[i][j] = 2;
+    if(chessBoard[i][j] == 0) {
+        oneStep(i, j, me);
+        chessBoard[i][j] = 1;
+        for (var k = 0; k < count; k++) {
+            if (wins[i][j][k]) {
+                myWin[k]++;
+                computerWin[k] = 6;
+                if (myWin[k] == 5) {
+                    window.alert("æ­å–œä½ ~ä½ èµ¢äº†ï¼ï¼");
+                    over = true;
+                }
+            }
         }
+        if(!over){
+            me = !me;
+            computerAI();
+        }
+    }
+}
+
+var computerAI = function(){
+    var myScore = [];
+    var computerScore = [];
+    var max = 0;
+    var u = 0, v = 0;
+
+    for(var i=0; i<15; i++){
+        myScore[i] = [];
+        computerScore[i] = [];
+        for(var j=0; j<15; j++){
+            myScore[i][j] = 0;
+            computerScore[i][j] = 0;
+        }
+    }
+    for(var i=0; i<15; i++){
+        for(var j=0; j<15; j++){
+            if(chessBoard[i][j] == 0){
+                for(var k=0; k<count; k++){
+                    if(wins[i][j][k]){
+                        if(myWin[k] == 1){
+                            myScore[i][j] += 200;
+                        }else if(myWin[k] == 2){
+                            myScore[i][j] += 400;
+                        }else if(myWin[k] == 3){
+                            myScore[i][j] +=2000;
+                        }else if(myWin[k] ==4){
+                            myScore[i][j] +=10000;
+                        }
+                        if(computerWin[k] == 1){
+                            computerScore[i][j] += 220;
+                        }else if(computerWin[k] == 2){
+                            computerScore[i][j] += 420;
+                        }else if(computerWin[k] == 3){
+                            computerScore[i][j] +=2100;
+                        }else if(computerWin[k] ==4){
+                            computerScore[i][j] +=20000;
+                        }
+                    }
+                }
+                if(myScore[i][j] > max){
+                    max = myScore[i][j];
+                    u = i;
+                    v = j;
+                }else if(myScore[i][j] == max){
+                    if(computerScore[i][j] > computerScore[u][v]){
+                        u = i;
+                        v = j;
+                    }
+                }
+                if(computerScore[i][j] > max){
+                    max = computerScore[i][j];
+                    u = i;
+                    v = j;
+                }else if(computerScore[i][j] == max){
+                    if(myScore[i][j] > myScore[u][v]){
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+        }
+    }
+    oneStep(u, v, false);
+    chessBoard[u][v] = 2;
+    for (var k = 0; k < count; k++) {
+        if (wins[u][v][k]) {
+            computerWin[k]++;
+            myWin[k] = 6;
+            if (computerWin[k] == 5) {
+                window.alert("å“ˆå“ˆ~ä½ è¾“äº†ï¼");
+                over = true;
+            }
+        }
+    }
+    if(!over){
         me = !me;
     }
 }
+
+
+
+
+
+
 
 
 
